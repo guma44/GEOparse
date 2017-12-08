a17b2a603edcfd102792434f16bf8adda9a9062c
Rafal Gumienny <guma44@gmail.com>
2017-12-08 15:00:06 +0100

    chore: Preparation for release

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [
 
 setup(
     name='GEOparse',
-    version='1.0.2',
+    version='1.0.3',
     description="Python library to access Gene Expression Omnibus Database (GEO)",
     long_description=readme + '\n\n' + history,
     author="Rafal Gumienny",
