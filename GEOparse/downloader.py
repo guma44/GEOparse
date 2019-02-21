e84cc48c83e65eda01da432c54c319ef117659b6
Rafal Gumienny <guma44@gmail.com>
2019-02-21 17:03:02 +0100

    feat: Make downloader class

--- GEOparse/downloader.py
+++ GEOparse/downloader.py
@@ -7,48 +7,44 @@ from urlparse import urlparse
 
 from .logger import geoparse_logger as logger
 
-def get_filename(url):
-    filename = os.path.basename(urlparse(url).path).strip(" \n\t.")
-    if len(filename) == 0:
-        raise Exception("Cannot parse filename from %s" % url)
-    return filename
+class Downloader(object):
+    """Downloader class."""
+    def __init__(self, url, outdir, filename=None, silent=False):
+        self.url = url
+        if outdir is None:
+            outdir = os.getcwd()
+        if filename is None:
+            filename = self._get_filename()
+        self.silent = silent
+    
+    @property
+    def destination(self):
+        """Get the destination path.
+        
+        This is the property should be calculated every time it is used because
+        a user could change the outdir and filename dynamically.
+        """
+        return os.path.join(os.path.abspath(self.outdir), self.filename)
 
-def download(url, outdir, filename=None, silent=False):
-    """Download from URL.
+    def download(self):
+        """Download from URL."""
+        logger.info("Downloading %s to %s" % (self.url, self.destination))
+        if self.url.startswith("http"):
+            self._download_http()
+        elif self.url.startswith("ftp"):
+            self._download_ftp()
+        else:
+            raise ValueError("Invalid URL %s" % self.url)
+        
+        
+    def _get_filename(self):
+        filename = os.path.basename(urlparse(self.url).path).strip(" \n\t.")
+        if len(filename) == 0:
+            raise Exception("Cannot parse filename from %s" % self.url)
+        return filename
 
-    :param url: URL - could be FTP or HTTP
-    :type url: :class:`str`
-    :param outdir: Path to the outdir file
-    :type outdir: :class:`str`
-    :raises: ValueError if URL is invalid
-    """
-    if filename is None:
-        filename = get_filename(url)
-    destination = os.path.join(outdir, filename)
-    logger.info("Downloading %s to %s" % (url, destination))
-    if url.startswith("http"):
-        r = requests.get(url, stream=True)
-        # Total size in bytes.
-        total_size = int(r.headers.get('content-length', 0))
-        chunk_size = 1024
-        with open(destination, 'wb') as f:
-            if silent:
-                for chunk in r.iter_content(chunk_size):
-                    if chunk:
-                        f.write(chunk)
-            else:
-                with tqdm(
-                        total=total_size,
-                        unit="B",
-                        unit_scale=True,
-                        unit_divisor=1024,
-                        leave=True) as pbar:
-                    for chunk in r.iter_content(chunk_size):
-                        if chunk:
-                            f.write(chunk)
-                            pbar.update(len(chunk))
-    elif url.startswith("ftp"):
-        parsed_url = urlparse(url)
+    def _download_ftp(self):
+        parsed_url = urlparse(self.url)
         try:
             ftp = FTP(parsed_url.netloc)
             ftp.login()
@@ -69,10 +65,29 @@ def download(url, outdir, filename=None, silent=False):
         except Exception:
             try:
                 ftp.quit()
-                logger.error("Error when trying to retreive %s." % url,
+                logger.error("Error when trying to retreive %s." % self.url,
                              exc_info=True)
             except Exception:
                 logger.error("Error when quiting FTP server.", exc_info=True)
 
-    else:
-        raise ValueError("Invalid URL %s" % url)
+    def _download_http(self):
+        r = requests.get(self.url, stream=True)
+        # Total size in bytes.
+        total_size = int(r.headers.get('content-length', 0))
+        chunk_size = 1024
+        with open(destination, 'wb') as f:
+            if self.silent:
+                for chunk in r.iter_content(chunk_size):
+                    if chunk:
+                        f.write(chunk)
+            else:
+                with tqdm(
+                        total=total_size,
+                        unit="B",
+                        unit_scale=True,
+                        unit_divisor=1024,
+                        leave=True) as pbar:
+                    for chunk in r.iter_content(chunk_size):
+                        if chunk:
+                            f.write(chunk)
+                            pbar.update(len(chunk))
